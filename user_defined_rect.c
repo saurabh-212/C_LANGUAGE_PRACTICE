63ea2eb541f03158059e11a88e55344d2caeea7c
saurabh-212 <95900659+saurabh-212@users.noreply.github.com>
2022-02-27 20:22:57 +0530

    Create user_defined_rect.c

---
+++ user_defined_rect.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int length, breadth;
+    printf("What is the length of your rectangle: \n");
+    scanf("%d", &length);
+
+    printf("What is the breadth of your rectangle: \n");
+    scanf("%d", &breadth);
+
+    printf("The area of your rectangle is: %d\n",length*breadth);
+
+    return 0;
+}
